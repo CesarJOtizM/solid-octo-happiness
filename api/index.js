ef2625e5a82ddd569c631f8a3149074decc93ef1
Cesar Ortiz <cesarjavierortizmontero@gmail.com>
2025-09-19 09:02:08 -0500

    fix deploy

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 // Punto de entrada para Vercel
-const app = require("../dist/app.js").default;
+const app = require("../dist/app.js");
 
 // Exportar la aplicación para Vercel
 module.exports = app;
