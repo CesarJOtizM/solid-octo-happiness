78e08a5dce6859096266241504ece99335239751
Cesar Ortiz <cesarjavierortizmontero@gmail.com>
2025-09-19 10:02:03 -0500

    fix deploy

---
+++ api/app.js
@@ -0,0 +1,2 @@
+// Wrapper para Vercel que usa el archivo compilado
+module.exports = require("../dist/app.js");
--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Punto de entrada para Vercel
-const app = require("../dist/app.js").default;
-
-// Exportar la aplicación para Vercel
-module.exports = app;
